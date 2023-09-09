7590680c447077083f473e5d301d895d803a924d
Bedo <bedonassef71@gmail.com>
2023-09-10 01:41:43 +0300

    enhance login & register

--- src/auth/utils/helpers/plain-into-user.response.ts
+++ src/auth/utils/helpers/plain-into-user.response.ts
@@ -1,6 +1,7 @@
 import { UserResponseDto } from '../../../users/dto/user-response.dto';
+import {UserDocument} from "../../../users/entities/user.entity";
 
-export function plainIntoUserResponse(user: any): UserResponseDto {
+export function plainIntoUserResponse(user: UserDocument): UserResponseDto {
   return {
     id: user.id,
     username: user.username,
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { User } from './entities/user.entity';
+import {User, UserDocument} from './entities/user.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { SignUpDto } from '../auth/dto/sign-up.dto';
@@ -10,17 +10,17 @@ export class UsersService {
     @InjectModel(User.name) private readonly userModel: Model<User>,
   ) {}
 
-  create(signUpDto: SignUpDto): Promise<User> {
+  create(signUpDto: SignUpDto): Promise<UserDocument> {
     return this.userModel.create(signUpDto);
   }
 
-  async findById(id: string): Promise<User> {
-    const user: User | undefined = await this.userModel.findById(id);
+  async findById(id: string): Promise<UserDocument> {
+    const user: UserDocument | undefined = await this.userModel.findById(id);
     if (!user) throw new NotFoundException('user not found');
     return user;
   }
 
-  findByEmail(email: string): Promise<User | undefined> {
+  findByEmail(email: string): Promise<UserDocument | undefined> {
     return this.userModel.findOne({ email });
   }
 }
